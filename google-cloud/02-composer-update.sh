8e2c1e61ae952f8441d4d5a60619f09773969a0b
durgaprasad.potnuru <85012836@bat.com>
2020-05-24 19:24:40 +0530

    Included commands for app engine deploy

--- google-cloud/02-composer-update.sh
+++ google-cloud/02-composer-update.sh
@@ -1,5 +1,11 @@
 #run these commands from directory where existing wordpress files are copied to
 #ex: ..\wordpress\migratefiles
-sudo apt-get install php7.2-zip php7.2-curl
+#sudo add-apt-repository ppa:ondrej/php
+#sudo apt-get update
+#sudo apt-get install php7.2
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
 composer require google/cloud-tools
 php vendor/bin/wp-gae update ../migratefiles
